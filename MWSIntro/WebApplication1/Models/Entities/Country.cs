d091bc3bd9e10338da66f8bb123bcc36d4b5198c
AliNazarovv <alirn@code.edu.az>
2024-05-28 17:54:39 +0400

    tamamlandi

---
+++ MWSIntro/WebApplication1/Models/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+
+    }
+}
